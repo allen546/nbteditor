8af58f431cc61ed6c67c9ab50082af4caf90d5f5
Allen Sun <allen.haha@hotmail.com>
2024-12-21 18:35:40 +0800

    ready for readme.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# NBTEditor
+
+## Video Demo: [link]{}
+
+## Description
+
+This is a Minecraft NBT Formatted File Editor.
+What is NBT (Named Binary Tag) Format? [See This](https://minecraft.fandom.com/wiki/NBT_format)
+
+## How to use
+
+### Install
+
+Run the following command:
+`pip3 install -r requirements.txt`
+
+### Run
+
+Run `editor.py` like this:
+`python3 editor.py [filename]`
+
+Example:
+`python3 editor.py test-player.dat`
--- editor.py
+++ editor.py
@@ -2,255 +2,22 @@ import sys
 import amulet_nbt
 import warnings
 import pprint
+import itertools
 from mutf8 import decode_modified_utf8, encode_modified_utf8
 warnings.simplefilter("ignore")
 
-class Item():
-    def __init__(self, count: int, item_id: str, tags: dict):
-        self.count = count
-        self.item_id = item_id
-        self.tags = {name: obj for name, obj in tags.items()}
-        self.enchantments = None
-        self.parse_enchantments()
-        
-    @classmethod
-    def from_nbt_tag(cls, tag: amulet_nbt.CompoundTag):
-        try:
-            tags = dict(tag.get_compound("tag"))
-        except:
-            try:
-                tags = dict(tag.get_compound("Stack").get_compound("tag"))
-            except:
-                tags = dict()
-        if "BlockEntityTag" in tags.keys():
-            try:
-                bet = list(tags["BlockEntityTag"].get_list("Items"))
-            except: pass
-            else:
-                return ShulkerBox(count=tag.get_byte("Count").py_int,
-                item_id=tag.get_string("id").py_str,
-                tags=tags, inv=bet)
-        if "Inventory" in tags.keys() and tag.get_string("id").py_str.endswith("backpack"):
-            bet = list(tags["Inventory"])
-            return Backpack(count=tag.get_byte("Count").py_int,
-            item_id=tag.get_string("id").py_str,
-            tags=tags, inv=bet)
-        try:
-            stack = tag.get_compound("Stack")
-        except KeyError:
-            pass
-        else:
-            return cls(
-                count=tag.get_compound("Stack").get_byte("Count").py_int,
-                item_id=tag.get_compound("Stack").get_string("id").py_str,
-                tags=tags
-            )
-        return cls(
-            count=tag.get_byte("Count").py_int,
-            item_id=tag.get_string("id").py_str,
-            tags=tags
-        )
 
-    def parse_enchantments(self):
-        if "Enchantments" in self.tags.keys() or "StoredEnchantments" in self.tags.keys():
-            self.enchantments = dict()
-        else:
-            return
-        if "Enchantments" in self.tags.keys():
-            for ench in self.tags["Enchantments"].py_list:
-                self.enchantments[ench.get_string("id").py_str] = ench.get_short("lvl").py_int
-        if "StoredEnchantments" in self.tags.keys():
-            for ench in self.tags["StoredEnchantments"].py_list:
-                self.enchantments[ench.get_string("id").py_str] = ench.get_short("lvl").py_int
-
-    def __repr__(self):
-        return f"{type(self).__name__}(\"{self.item_id}\") x {self.count}"
-
-class InventoryItem(Item):
-    def set_slot(self, slot):
-        self.slot = slot
-
-class Inventory:
-    _slots_ = [
-        100, 101, 102, 103
-    ] + list(range(32))
-
-    def __init__(self, inv: list):
-        self.inv = {}
-        for i in inv:
-            try:
-                slot = i.get_byte("Slot").py_int
-            except:
-                slot = i.get_int("Slot").py_int
-            self.inv[slot] = InventoryItem.from_nbt_tag(i)
-            self.inv[slot].set_slot(slot)
-
-
-    def get_item_by_slot(self, slot):
-        return self.inv[slot]
-
-    @classmethod
-    def load_from_tag(cls, tag: amulet_nbt.ListTag):
-        return cls(list(tag))
-
-    def __repr__(self):
-        s = type(self).__name__ + f"(\n\t"
-        for b, i in self.inv.items():
-            s += str(b) + ": " + str(i) + "\n\t"
-        s += ")"
-        return s
-
-class EnderChest(Inventory):
-    _slots_ = list(range(28))
-
-class ShulkerBox(Inventory, InventoryItem):
-    _slots_ = list(range(28))
-    def __init__(self, count, item_id, tags, inv):
-        Inventory.__init__(self, inv)
-        InventoryItem.__init__(self, count, item_id, tags)
-
-class Backpack(ShulkerBox):
-    _slots_ = []
-
-
-class Coord():
-    def __init__(self, x, y, z, dimension):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.demension = dimension
-        self.d = dimension
-
-    def __repr__(self):
-        return f"Coord({self.x}, {self.y}, {self.z}, {repr(self.d)})"
-
-class Player():
-    def __init__(self, filename="player.dat"):
-        data = amulet_nbt.load(
-            filename,
-            string_decoder=decode_modified_utf8
-        )
-        self.data = dict(data.compound)
-
-        self.pos = Coord(
-            self.data["Pos"][0].py_float, 
-            self.data["Pos"][1].py_float, 
-            self.data["Pos"][2].py_float, 
-            self.data["Dimension"].py_str
-            )
-        self.health = int(self.data["Health"].py_float)
-        self.hunger = self.data["foodLevel"].py_int
-        self.saturation = self.data["foodSaturationLevel"].py_float
-        self.inventory = Inventory.load_from_tag(self.data["Inventory"])
-        self.ender_chest = EnderChest.load_from_tag(self.data["EnderItems"])
-
-    def __repr__(self):
-        return f"Player(pos={self.pos}, {self.health}/20HP, {self.hunger}+{self.saturation}/20SATU, inv=\n{self.inventory},\nender_chest=\n{self.ender_chest})"
-
-
-class NBT:
-    def __init__(self, tag: amulet_nbt.AnyNBT):
-        self.tag = tag
-        self.typeof = type(tag)
-
-    def save(self, filename):
-        self.tag.save_to(filename, compressed=False, string_encoder=encode_modified_utf8)
-
-class DataTag(NBT):
-    def __init__(self, tag: amulet_nbt.AnyNBT):
-        super().__init__(tag)
-
-    @property
-    def value(self):
-        return self.tag.py_data
-    
-    @value.setter
-    def value(self, value):
-        self.tag = self.typeof(value)
-
-    def __repr__(self):
-        return self.value
-    
-class RootTag(NBT):
-    def __init__(self, tag: amulet_nbt.NamedTag):
-        super().__init__(tag)
-    
-    @property
-    def value(self):
-        return self.tag.compound
-    
-
-class ListTag(DataTag):
-    @property
-    def value(self):
-        return self.tag.py_data
-    
-    @value.setter
-    def value(self, value):
-        self.tag = self.typeof(value)
-    
-    def __getitem__(self, i):
-        return self.tag[i]
-    
-    def get(self, i=None):
-        if i is None:
-            return self.tag
-        return self.tag[i]
-    
-    def set(self, i, val):
-        v = self.value
-        v[i] = val
-        self.value = v
-
-    def __repr__(self):
-        return repr(self.value)
-
-class CompoundTag(DataTag):
-    @property
-    def value(self):
-        return self.tag.py_data
-    
-    @value.setter
-    def value(self, value):
-        self.tag = self.typeof(value)
-    
-    def __getitem__(self, i):
-        return self.tag[i]
-    
-    def get(self, i=None):
-        if i is None:
-            return self.tag
-        return self.tag[i]
-    
-    def set(self, i, val):
-        v = self.value
-        v[i] = val
-        self.value = v
-
-    def __repr__(self):
-        return repr(dict(self.value))
-
-class IntArrayTag(DataTag):
-    @property
-    def value(self):
-        return list(self.tag.py_data)
-    
-    def as_hex(self):
-        return "-".join(hex(i)[2:] for i in self.value)
-    
-    @value.setter
-    def value(self, value):
-        self.tag = self.typeof(value)
-
-    def __repr__(self):
-        return self.as_hex()
 
 DATA_TYPES = [
-    ([amulet_nbt.ByteTag, amulet_nbt.StringTag, amulet_nbt.IntTag, 
-      amulet_nbt.ShortTag, amulet_nbt.IntArrayTag, amulet_nbt.DoubleTag], DataTag),
-    ([amulet_nbt.ListTag], ListTag),
-    ([amulet_nbt.CompoundTag, amulet_nbt.NamedTag], CompoundTag),
+    (amulet_nbt.ByteTag, amulet_nbt.StringTag, amulet_nbt.IntTag, amulet_nbt.LongTag, 
+      amulet_nbt.ShortTag, amulet_nbt.IntArrayTag, amulet_nbt.DoubleTag, amulet_nbt.ByteArrayTag),
+    (amulet_nbt.ListTag,),
+    (amulet_nbt.CompoundTag, amulet_nbt.NamedTag),
 ]
+
+INPUT_DATA_TYPES = {i: j for i, j in zip(
+    range(len(list(itertools.chain(*DATA_TYPES)))-1), list(itertools.chain(*DATA_TYPES))[:-1]
+    )}
 
 class DoesNotExist(Exception): 
     pass
@@ -278,9 +45,36 @@ class NBTWalker:
     
     def set_tag(self, location, value):
         return self._set_tag(self._process_location(location), value)
+
+    def insert_tag(self, location, tagname, tagtype, value):
+        return self._insert_tag(self._process_location(location), tagname, tagtype, value)
+    
+    def create_tag(self, location, tagname, tagtype):
+        return self._create_tag(self._process_location(location), tagname, tagtype)
     
     def save(self, filename, compressed=False):
         self.root.save_to(filename, compressed=compressed, string_encoder=encode_modified_utf8)
+
+    def del_tag(self, location):
+        return self._del_tag(self._process_location(location))
+    
+    def _del_tag(self, location):
+        tag = self.root
+        if self._exists(location):
+            locs = []
+            for loc in location:
+                try:
+                    tag[loc]
+                except TypeError:
+                    loc = int(loc)
+                tag = tag[loc]
+                locs.append(loc)
+        else:
+            raise DoesNotExist(repr(location))
+        tag = self.root
+        for loc in locs[:-1]:  # I found out how to modify nested MutableSequence from an idea gotten from https://yiyan.baidu.com/
+            tag = tag[loc]     # Other similar methods below used the same idea
+        del tag[locs[-1]]
     
     def _set_tag(self, location, value):
         tag = self.root
@@ -300,7 +94,46 @@ class NBTWalker:
         for loc in locs[:-1]:
             tag = tag[loc]
         tag[locs[-1]] = typeof(value)
-        
+
+    def _insert_tag(self, location, tagname, tagtype, value):
+        tag = self.root
+        if self._exists(location):
+            locs = []
+            for loc in location:
+                try:
+                    tag[loc]
+                except TypeError:
+                    loc = int(loc)
+                tag = tag[loc]
+                locs.append(loc)
+        else:
+            raise DoesNotExist(repr(location))
+        tag = self.root
+        for loc in locs:
+            tag = tag[loc]
+        tag[tagname] = tagtype(value)
+
+    def _create_tag(self, location, tagname, tagtype):
+        tag = self.root
+        if self._exists(location):
+            locs = []
+            for loc in location:
+                try:
+                    tag[loc]
+                except TypeError:
+                    loc = int(loc)
+                tag = tag[loc]
+                locs.append(loc)
+        else:
+            raise DoesNotExist(repr(location))
+        tag = self.root
+        for loc in locs:
+            tag = tag[loc]
+        if tagname is None:
+            tag.append(tagtype.create())
+        else:
+            tag[tagname] = tagtype.create()
+
     def _get_tag(self, location):
         tag = self.root
         if self._exists(location):
@@ -334,7 +167,7 @@ class NBTWalker:
 def enter(command):
     global location
     if location:
-        newlocation = location + "." + command[1]
+        newlocation = location + "." + " ".join(command[1:])
     else:
         newlocation = command[1]
     if command[1].startswith("root."):
@@ -365,15 +198,18 @@ def exit(command):
 def do_list_tag(command):
     global location
     tag = t.get_tag(location)
-    if type(tag) in DATA_TYPES[1][0]:
-        print(f"List[{type(tag.value[0]).__name__}][0:{len(tag)}]")
-    elif type(tag) in DATA_TYPES[2][0]:
+    if type(tag) in DATA_TYPES[1]:
+        all_types = set(type(t).__name__ for t in tag.value)
+        print(f"List[{', '.join(all_types)}][0:{len(tag)-1}]")
+    elif type(tag) in DATA_TYPES[2]:
         for i, j in tag.value.items():
-            if type(j) in DATA_TYPES[0][0]:
+            if type(j) in DATA_TYPES[0]:
                 print("-", i, ":", j.value ,"("+type(j).__name__+")")
             else:
                 print("-", i, ":", type(j).__name__)
-    elif type(tag) == DataTag:
+        if len(tag.value) == 0:
+            print("Empty!")
+    elif type(tag) in DATA_TYPES[0]:
         print(f"Error: {location[-1]}: is an object")
     else:
         print(f"Error: unknown type {type(tag)}")
@@ -405,7 +241,10 @@ while True:
         elif len(command) == 2:
             if enter(["enter", command[1]]) != -1:
                 print(pprint.pformat(t.get_tag(location).value))
-                exit(["exit", len(command[1].split("."))])
+                if command[1].startswith("root."):
+                    exit(["exit", len(command[1].split("."))-1])
+                else:
+                    exit(["exit", len(command[1].split("."))])
 
     elif command[0] == "list":
         do_list_tag(command)
@@ -420,6 +259,40 @@ while True:
             else:
                 command[1] = location + "." + command[1]
             t.set_tag(command[1], command[2])
+
+    elif command[0] == "del":
+        if len(command) == 2:
+            if command[1].startswith("root."):
+                t.del_tag(command[1][5:])
+            else:
+                t.del_tag(location + "." + command[1])
+
+    # TODO: add ability to add ListTag and CompoundTag
+    elif command[0] == "insert":
+        # insert into data structure, too many args, ask for them
+        typeof = INPUT_DATA_TYPES[int(input("Type of data: \n"+"\n".join(
+            str(i)+": "+j.__name__ for i, j in INPUT_DATA_TYPES.items()) + "\n-> "
+            ))]
+        if typeof in DATA_TYPES[0]:
+            name = input("Tag name: ")
+            val = input("Value: ")
+            t.insert_tag(location, name, typeof, val)
+        else:
+            if type(t.get_tag(location)) in DATA_TYPES[2]:
+                name = input("Tag name: ")
+                t.create_tag(location, name, typeof)
+            else:
+                name = str(len(t.get_tag(location)))
+                t.create_tag(location, None, typeof)
+
+        print("Created tag", location + "." + name)
+
+    # TODO: add as_hex command
+    elif command[0] == "as_hex":
+        cur = t.get_tag(location)
+        if isinstance(cur, amulet_nbt.IntArrayTag):
+            print("-".join(hex(i)[2:] for i in cur.py_data))
+    
     elif command[0] == "save":
         if len(command) == 2:
             t.save(command[1])
@@ -430,6 +303,21 @@ while True:
                 print(type(e), e.args[0])
         else:
             print("Usage: save `filename` [compressed=0]")
+
+    elif command[0] == "help":
+        print("""
+Commands:
+quit - quit immediately
+enter [tag] - enter [tag]
+exit [layers] - exit [layers] layers or 1 layer (default)
+list [location] - list all tags under ([location] | current location)
+value [location] - get the value of ([location] | current location)
+save filename [compressed=0] - save opened file to "filename" with compression=compressed
+del [location] - delete location from file (NotImplemented)
+insert - insert into current location a tag (other values are prompted)
+as_hex - represent current value as hexadecimal     
+        (Note: this is raw UUID, different from standard Minecraft UUID format, only 1st section is the same)
+""")
     else:
         print(f"Unknown command: \'{command[0]}\'")
 
